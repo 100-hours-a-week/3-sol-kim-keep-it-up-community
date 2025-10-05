0aef79e4ab61000c60a5585ed9e2fc6c700ca436
Jimin Kim <0305jimin@gmail.com>
2025-10-05 22:50:43 +0900

    fix: resolving 'userService is null' error

--- community/src/main/java/com/project/community/controller/UserController.java
+++ community/src/main/java/com/project/community/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserSignUpRequest userSignUpRequest) {
