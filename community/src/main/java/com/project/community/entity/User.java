91e71b3f1d0d81e43d9d1caf19574647a5986ea4
Jimin Kim <0305jimin@gmail.com>
2025-10-05 22:17:39 +0900

    feat: adding entities - User, Post

---
+++ community/src/main/java/com/project/community/entity/User.java
@@ -0,0 +1,33 @@
+package com.project.community.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id // PK(기본키) 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY : Id가 null일 경우 해당 객체의 Id를 DB의 자동 증가 컬럼으로 가져와 할당(DB 벤더에 의존적, 기본키 생성을 DB에게 위임, 유일성 보장)
+    @Column(name = "comment_id") // 데이터베이스에 어떤 이름과 형식으로 저장될지 지정. 이름, 데이터 형식, 제약 조건 등을 명시
+    private Long id;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String nickname;
+
+    @Email(message = "이메일 형식이 아닙니다.")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // private String profilImageUrl;
+
+    @OneToMany(mappedBy = "writer")
+    private List<Post> postList = new ArrayList<>();
+
+    private boolean didWithdraw = false;
+
+}
