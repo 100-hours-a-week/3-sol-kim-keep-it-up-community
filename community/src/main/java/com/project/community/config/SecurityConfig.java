d4fe4fcf38f21bc686e4f74d732c0e12c4d74281
Jimin Kim <0305jimin@gmail.com>
2025-10-14 17:45:43 +0900

    fix: CORS related error 'No 'Access-Control-Allow-Origin' header is present on the requested resource'

--- community/src/main/java/com/project/community/config/SecurityConfig.java
+++ community/src/main/java/com/project/community/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.project.community.config;
 
+import org.springframework.web.filter.CorsFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
 
     @Bean // register the object to be returned to IoC
     public BCryptPasswordEncoder encode() {
@@ -18,6 +24,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter) // every request passes through the filter
+                .formLogin().disable() // not using form tag, original login style
+                .httpBasic().disable();
         http.authorizeHttpRequests(authorize -> authorize
                 // specific request url to be protected
                 // for other requests, permit all
