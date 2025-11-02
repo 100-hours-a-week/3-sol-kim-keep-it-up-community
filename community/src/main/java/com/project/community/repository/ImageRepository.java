f32414484bebf55d2c4c164f757f39b22117bce6
Jimin Kim <0305jimin@gmail.com>
2025-11-03 01:18:25 +0900

    chore: code for test

--- community/src/main/java/com/project/community/repository/ImageRepository.java
+++ community/src/main/java/com/project/community/repository/ImageRepository.java
@@ -3,8 +3,12 @@ package com.project.community.repository;
 import com.project.community.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Image findByTypeAndUserId(String profile, Long userId);
 
     Image findByTypeAndPostId(String post, Long postId);
+
+    List<Image> findAllByTypeAndUserId(String profile, Long userId);
 }
