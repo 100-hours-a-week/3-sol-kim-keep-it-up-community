cb0cf490615c4ee93878a13cd908c16dc87662d7
Jimin Kim <0305jimin@gmail.com>
2025-11-05 09:33:58 +0900

    fix: omitted component annotation

--- community/src/main/java/com/project/community/util/ErrorResponseWriter.java
+++ community/src/main/java/com/project/community/util/ErrorResponseWriter.java
@@ -2,9 +2,11 @@ package com.project.community.util;
 
 import com.project.community.common.ErrorCode;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class ErrorResponseWriter {
     public static void writeError(HttpServletResponse response, ErrorCode code) throws IOException {
         response.setStatus(code.getHttpStatus().value());
